9e7627d02b69864d526b3f89e89f4d0cbe2be83e
Rade <04-unshod-clashes@icloud.com>
2024-02-03 18:07:37 +0100

    dodatak na prethodni commit

---
+++ RadivojeBjelic_DeltaDrive_Delta/Domain/DriverDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Domain
+{
+    public class DriverDTO
+    {
+        public Guid Driver_ID { get; set; }
+        public string Brand { get; set; }
+        public string Driver { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string StartPrice { get; set; }
+        public string PricePerKM { get; set; }
+    }
+}
+
